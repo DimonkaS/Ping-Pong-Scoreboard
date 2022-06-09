869ffcec8844a7425abc22bad924e66449ca1cb7
DimonkaS <91775336+DimonkaS@users.noreply.github.com>
2022-06-09 18:26:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pingpongscoreboard/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pingpongscoreboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowInsetsController
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main)
+        this.supportActionBar?.hide()
+
+    }
+}
